48acfc95f98cad454603b91f546e82eacfb190a7
mimi-katsu <maiamor1000@gmail.com>
2024-03-26 23:07:52 -0700

    added some basic argumnent parsing and the option to decode messages from base64

--- main.py
+++ main.py
@@ -1,55 +1,74 @@
 import asyncio
 import argparse
-
+import base64
 # parser = parser.parse_args
 from dns import DNSRequest
 
+parser = argparse.ArgumentParser(description='Start a DNS server for extracting secret messages from DNS requests')
+
+parser.add_argument('-i', default='0.0.0.0', help='Specify the address to listen on')
+parser.add_argument('-p', type=int, default=53, help='Specify the port to listen on')
+parser.add_argument('--decode', default=False, help='Decode messages from base64', action='store_true')
+
+args = parser.parse_args()
+
 class DNSKitty:
-    def __init__(self):
+    def __init__(self, args):
         self.stopped = False
+        self.args = args
 
     class DNS:
-        def __init__(self):
+        def __init__(self, args):
             self.transport = None
+            self.args = args
 
         def connection_made(self, transport):
-            print('Listening...')
             self.transport = transport
 
         def datagram_received(self, data, address):
-            self.parse_data(data, address)
+            self.parse_data(data, address, self.args)
             self.transport.sendto(data, address)
 
         def get_message(self, request):
             message = request.question.qname.split('.')[:-2]
             return message
 
-        def parse_data(self, data, address):
+        def parse_data(self, data, address, args):
             request = DNSRequest(data)
-            print(f'Message recieved from {address[0]}: {self.get_message(request)}')
+            msg = ""
+            if args.decode:
+                for m in self.get_message(request):
+                    msg += base64.b64decode(m).decode().strip()
+                    
+            else:
+                for m in self.get_message(request):
+                    msg += m
+            print(f'Message recieved from {address[0]}: {msg}')
 
     async def start_server(self, address:str, port:int):
-        loop = asyncio.get_running_loop()
-
-        transport, protocol = await loop.create_datagram_endpoint(
-            lambda: self.DNS(),
-            local_addr = (address, port)
-            )
-
         try:
-            while not self.stopped:
-                await asyncio.sleep(.0000001)
+            loop = asyncio.get_running_loop()
+            transport, protocol = await loop.create_datagram_endpoint(
+                lambda: self.DNS(self.args),
+                local_addr = (self.args.i, self.args.p)
+                )
+            print(f'Listening on {self.args.i}:{self.args.p}')
+            try:
+                while not self.stopped:
+                    await asyncio.sleep(.0000001)
 
-        except KeyboardInterrupt:
-            self.stopped = True
-            print(f'Stopping: {self.stopped}')
+            except KeyboardInterrupt:
+                self.stopped = True
+                print(f'Stopping: {self.stopped}')
 
-        finally:
-            transport.close()
-            print('Transport  closed')
-    
+            finally:
+                transport.close()
+                print('Transport  closed')
+        except OSError:
+            print("address already in use or try running with 'sudo'")
+
 async def main():
-    server = DNSKitty()
+    server = DNSKitty(args)
     await server.start_server('127.0.0.1', 53)
 
 if __name__ == '__main__':
